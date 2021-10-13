f20112b430ef0444e207ca982a7fbce756c69781
ethelgarcia <70079570+ethelgarcia@users.noreply.github.com>
2021-10-13 10:46:38 -0500

    Ethel commit 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,flash, render_template, request
 
 app = Flask(__name__)
 app.debug = True
@@ -6,7 +6,42 @@ app.debug = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    try:
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+
+            #db = get_db()
+            error = None
+
+            if not username:
+                error = 'Debes ingresar un usuario'
+                flash(error)
+                return render_template('login.html')
+
+            if not password:
+                error = 'Debes ingresar una contraseña'
+                flash(error)
+                return render_template('login.html')
+
+            #user = db.execute('SELECT * FROM usuario WHERE usuario= ? AND contraseña= ?', (username, password)).fetchone()
+            user=None
+            if user is None:
+                error = 'Usuario o contraseña inválidos'
+                flash(error)
+            else:
+                return render_template('mensaje')
+
+            #db.close_db()
+
+        return render_template('login.html')
+    except Exception as ex:
+        print(ex)
+        return render_template('login.html')
 
 
 @app.route('/createuser')
@@ -22,6 +57,19 @@ def createproduct():
 @app.route('/createprovider')
 def createprovider():
     return render_template('createprovider.html')
+
+@app.route('/createproduct_usuariofinal')
+def createproduct_usuariofinal():
+    return render_template('createproduct_usuariofinal.html')
+
+
+@app.route('/createprovider_usuariofinal')
+def createprovider_usuariofinal():
+    return render_template('createprovider_usuariofinal.html')
+
+@app.route('/editareliminarproducto') #ETHEL
+def editareliminarprovider():
+    return render_template('editareliminarproducto.html')
 
 #dsdsd
 if __name__ == '__main__':
